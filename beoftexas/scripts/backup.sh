1a395b321cd6ac26334c7e3acd8c631fb305ac13
Clay Smith <casmith@users.noreply.github.com>
2025-02-08 12:29:44 -0600

    clean up after backup

--- beoftexas/scripts/backup.sh
+++ beoftexas/scripts/backup.sh
@@ -4,7 +4,16 @@ set -e
 SCRIPT_DIR=$(dirname $0)
 WORK_DIR=$SCRIPT_DIR/backup-temp
 mkdir --parents $WORK_DIR
+
+# clean up any previously failed run
 rm -rf $WORK_DIR/*
+
+# run the backup
 docker run --rm -v $WORK_DIR:/mysqldump -e DB_USER='beoftexas' -e DB_NAME=beoftexas -e DB_PASS=$(cat $SCRIPT_DIR/secret/beoftexas_db_pw.txt) -e DB_HOST=work_db_1 --network work_default casmith/mysqldump
 cp $WORK_DIR/*.* $WORK_DIR/beoftexas_latest.sql.gz
+
+# upload
 for filename in $WORK_DIR/*.sql.gz; do aws s3 cp $filename s3://beoftexas-backup/$(basename $filename); done
+
+# clean up
+rm -rf $WORK_DIR/*
