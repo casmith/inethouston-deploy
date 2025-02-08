e1c9bce05ea4fe0abe48732a94cb1fcfe5dee302
Clay Smith <casmith@users.noreply.github.com>
2025-02-08 11:47:29 -0600

    fix backup script

--- beoftexas/scripts/backup.sh
+++ beoftexas/scripts/backup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -e
 
-rm -rf backup-temp/*
-docker run --rm -v ./backup-temp:/mysqldump -e DB_USER='beoftexas' -e DB_NAME=beoftexas -e DB_PASS=$(cat beoftexas/secret/beoftexas_db_pw.txt) -e DB_HOST=work_db_1 --network work_default casmith/mysqldump
-cp backup-temp/*.* backup-temp/beoftexas_latest.sql.gz
-for filename in ./backup-temp/*.sql.gz; do aws s3 cp $filename s3://beoftexas-backup/$(basename $filename); done
+SCRIPT_DIR=$(dirname $0)
+WORK_DIR=$SCRIPT_DIR/backup-temp
+mkdir --parents $WORK_DIR
+rm -rf $WORK_DIR/*
+docker run --rm -v $WORK_DIR:/mysqldump -e DB_USER='beoftexas' -e DB_NAME=beoftexas -e DB_PASS=$(cat $SCRIPT_DIR/secret/beoftexas_db_pw.txt) -e DB_HOST=work_db_1 --network work_default casmith/mysqldump
+cp $WORK_DIR/*.* $WORK_DIR/beoftexas_latest.sql.gz
+for filename in $WORK_DIR/*.sql.gz; do aws s3 cp $filename s3://beoftexas-backup/$(basename $filename); done
